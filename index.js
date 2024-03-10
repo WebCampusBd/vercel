ea220295eced22e10c9f363238e2d9e806b8b37f
Ratul Anjum <mdratulanjum@gmail.com>
2024-03-10 18:48:53 +0600

    2nd commit

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ app.use(express.json());
 // Routes
 app.use("/home", home);
 
+app.get("/", (req,res)=>{
+    res.status(200).send("<h1 align='center'>Hello This is me Ratul Anjum</h1>")
+})
+
 // connection
 const port = process.env.PORT || 9001;
 app.listen(port, () => console.log(`Listening to port ${port}`));
--- routes/home.js
+++ routes/home.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", async (req, res, next) => {
-  return res.status(200).json({
+   res.status(200).json({
     title: "Express Testing",
     message: "The app is working properly!",
   });
